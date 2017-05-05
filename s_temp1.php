460446444ec44da42d74f46f5a88bab75da4c938
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2017-05-04 22:43:44 -0700

    correct header tags

--- s_temp1.php
+++ s_temp1.php
@@ -56,7 +56,7 @@ path {
 </style>
 <body>
 
-<h1 "This is a header" /h1>
+<h1> "This is a header" </h1>
 
 <!-- load the d3.js library -->    
 <!--<script src="http://d3js.org/d4.v3.min.js"></script>-->
