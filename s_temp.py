8cbdbb17bcbc770a3397b8a1dd85a57e0ec5d513
Derek Sloan DerekHP2017 <dsloan@ibe-inc.com>
2018-03-13 17:49:36 -0700

    add WriteReadSQL and completely sync working dirs

--- s_temp.py
+++ s_temp.py
@@ -6,8 +6,8 @@ import glob
 import time
 import MySQLdb as mdb
  
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+os.system('/sbin/modprobe w1-gpio')
+os.system('/sbin/modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
