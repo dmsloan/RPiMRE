9b99e684f58f3bdffb3869d4180fae49de410747
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2018-03-31 23:18:23 -0700

    add examples of outputting to CSV

---
+++ exportToCSV.php
@@ -0,0 +1,62 @@
+<?php
+    //exportToCSV.php
+    // the following block exports the data to a csv file that can be imported to excel
+    // the contents of the file are ereased first
+
+if(isset($_POST["export"]))  
+{  
+    $hostname = 'Pi2WebServer4G';
+    $username = 'pi_select';
+    $password = 'raspberry';
+    $database = 'db_cpu';
+    $filename = 'userData.csv';
+
+    $dbh = new PDO("mysql:host=$hostname;dbname=$database", 
+    $username, $password);
+    
+    /*** The SQL SELECT statement ***/
+    $sth = $dbh->prepare("
+        SELECT  `dateTime`,`TValue`
+        FROM  `TAB_CPU`
+        ORDER BY `dateTime` desc
+        LIMIT 0,80
+    ");
+    $sth->execute();
+    
+    /* Fetch all of the remaining rows in the result set */
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    
+    /*** close the database connection ***/
+    $dbh = null;
+
+    
+    echo "The result is:<br><br>";
+    echo "The final result is:".print_r(array_values($result))."<br><br>";
+
+    echo "The array keys are:<br><br>";
+    echo print_r(array_keys($result[0]))."<br><br>";
+
+    echo "The output for the foreach is:<br><br>";
+    foreach($result as $row)       
+    {  
+         echo print_r($row);  
+    }       
+    
+    
+// the following block exports the data to a csv file that can be imported to excel
+// the contents of the file are ereased first
+header("Content-type: text/csv");       
+header("Content-Disposition: attachment; filename=$filename");       
+$output = fopen($filename, "w");       
+$header = array_keys($result[0]);       
+fputcsv($output, $header);
+fputcsv($output,"test");       
+foreach($result as $row)       
+{  
+     fputcsv($output, $row);  
+}       
+fclose($output);  
+
+
+}  
+?>
---
+++ exportToCSVHTML.php
@@ -0,0 +1,67 @@
+<?php  
+
+$hostname = 'Pi2WebServer4G';
+$username = 'pi_select';
+$password = 'raspberry';
+$database = 'db_cpu';
+$filename = 'userData.csv';
+
+$dbh = new PDO("mysql:host=$hostname;dbname=$database", 
+$username, $password);
+
+/*** The SQL SELECT statement ***/
+$sth = $dbh->prepare("
+    SELECT  `dateTime`,`TValue`
+    FROM  `TAB_CPU`
+    ORDER BY `dateTime` desc
+    LIMIT 0,80
+");
+$sth->execute();
+
+/* Fetch all of the remaining rows in the result set */
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+/*** close the database connection ***/
+$dbh = null;
+
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Export Mysql Table Data to CSV file in PHP</title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br /><br />  
+
+           <div class="container" style="width:900px;">  
+                <h2 align="center">Export Mysql Table Data to CSV file in PHP</h2>  
+                <h3 align="center">Pi CPU Temperature</h3>                 
+                <br />  
+                <form method="post" action="exportToCSV.php" align="center">  
+                     <input type="submit" name="export" value="CSV Export" class="btn btn-success" />  
+                </form>  
+                <br />  
+                <div class="table-responsive" id="CPUTemperature">  
+                     <table class="table table-bordered">  
+                          <tr>  
+                               <th width="25%">dateTime</th>  
+                               <th width="25%">TValue</th>  
+                          </tr>  
+                     <?php  
+                     foreach ($result as $row): 
+                     ?>  
+                          <tr>  
+                               <td><?php echo $row["dateTime"]; ?></td>  
+                               <td><?php echo $row["TValue"]; ?></td>  
+                          </tr>  
+                     <?php       
+                     endforeach
+                     ?>  
+                     </table>  
+                </div>  
+           </div>  
+      </body>  
+ </html>  
