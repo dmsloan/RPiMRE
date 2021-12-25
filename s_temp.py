5609f66cc6b93780ea190692e221180b276e4d6e
dmsloan from Dell Latitude <dsloan@ibeinc.com>
2021-12-25 09:41:31 -0800

    changes to work on the pi3webserver

--- s_temp.py
+++ s_temp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import os
@@ -21,43 +21,37 @@ def read_temp_raw():
  
 def read_temp():
     lines = read_temp_raw()
-#    print(lines)
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
-#     	 print (temp_string)
+        #temp_c = float(temp_string) / 1000.0
+        #return temp_c
+	    #print (temp_string)
         temp_f = int((((float(temp_string)/1000)* (9.0/5.0)) + 32)*100)
-#        print (temp_f)
-	return temp_f
-	
+        #print (temp_f)
+    return temp_f
+
+
 while True:
 	
     try:
         pi_temp = read_temp()
-        con = mdb.connect('localhost', 'pi_insert', 'raspberry', 'measurements')
-        
-        # # Check if connection was successful
-        # if (con):
-        #     # Carry out normal procedure
-        #     print "Connection successful"
-        # else:
-        #     # Terminate
-        #     print "Connection unsuccessful"
-        
+        con = mdb.connect(host='192.168.1.85', user='pi_insert',
+			  passwd='raspberry', db='measurements')
         cur = con.cursor()
-        # print(pi_temp)
+
         # after hours of looking the following line had to be changed from (pi_temp) to (pi_temp,). (pi_temp)
         # worked with python 2.6 but not with 2.7. Adding the comma to change it to (pi_temp,) forced the variable
         # to be a tuple and corrected the type error 
         cur.execute("""INSERT INTO temperature(temperature) VALUES(%s)""", (pi_temp,))
         con.commit()
     
-    except mdb.Error, e:
+    except mdb.Error as e:
         con.rollback()
-        print "Error %d: %s" % (e.args[0],e.args[1])
+        print ("Error %d: %s" % (e.args[0],e.args[1]))
         sys.exit(1)
     
     finally:    
@@ -65,4 +59,4 @@ while True:
             con.close()
 		
 	# print(read_temp())	
-	time.sleep(10)
+        time.sleep(60)
